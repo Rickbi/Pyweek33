0b2c1e10667021eefd447ee8096ad5a5aa1a63db
Rickbi <92495344+Rickbi@users.noreply.github.com>
2022-03-25 20:43:16 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from map import Map
 
-if __name__ == '__main__':
-    map = Map(1440,810, 45, 45)
+def main(resolution = (1440, 810)):
+    nx = 32
+    ny = 18
+    w = resolution[0]
+    h = resolution[1]
+    d = min(w//nx, h//ny)
+    map = Map(w, h, d, d)
     map.main_menu()
+
+if __name__ == '__main__':
+    main( (1280, 720) )
